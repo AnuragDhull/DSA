bc55fedd74bb31b5913f9ae9dc2434d66a38a3fa
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2024-06-27 10:32:37 +0530

    Add files via upload

---
+++ basics_2.java
@@ -0,0 +1,17 @@
+class Algebra
+{
+    int add(int a,int b)
+    {
+        int ans=a+b;
+        return ans;
+    }
+}
+
+public class basics_2 {
+    public static void main(String[] args) {
+        Algebra obj=new Algebra();
+
+        System.out.println(obj.add(10, 20));
+
+    }
+}
