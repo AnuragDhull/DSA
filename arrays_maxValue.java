98d008e59903a8cc4a40d36e1e95fab972fe122d
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2024-07-01 12:54:26 +0530

    Add files via upload

---
+++ arrays_maxValue.java
@@ -0,0 +1,15 @@
+public class arrays_maxValue {
+    public static void main(String[] args) {
+        int[] marks={-10,20,30,40,50};
+
+        int max=0;
+        for(int i=0;i<marks.length;i++)
+        {
+           if(marks[i]>max)
+           {
+            max=marks[i];
+           }
+        }
+        System.out.println(max);
+    }
+}
