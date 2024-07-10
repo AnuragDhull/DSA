7448b80a2c2dd5c5867d823bc910b59ddd3c62d7
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2024-07-10 11:34:28 +0530

    Add files via upload

---
+++ wrapper_class.java
@@ -0,0 +1,16 @@
+public class wrapper_class {
+    public static void main(String[] args) {
+        
+
+        /// Wrapper Classs ///
+        Integer i =Integer.valueOf(4);
+        System.out.println(i);
+
+        Float f = Float.valueOf(4);
+        System.out.println(f);
+
+        // as java is oops basic class so we have to convert this normal 
+        // integer or float into oops
+
+    }
+}
