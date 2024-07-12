efd3e46a63de9a0c595a00f3e38b4c9ada517460
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2024-07-12 10:34:47 +0530

    Add files via upload

---
+++ dsa_basics_2.java
@@ -0,0 +1,5 @@
+public class dsa_basics_2 {
+    public static void main(String[] args) {
+        // Time Complexity is a concept in computer science that deals with the quantification of the amount of time taken by a set of code or algorithm to process or run as a function of the amount of input. In other words, the time complexity is how long a program takes to process a given input. The efficiency of an algorithm depends on two parameters:
+    }
+}
